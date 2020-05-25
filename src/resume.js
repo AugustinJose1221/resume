d14ba1ce226b5a08912dac0c14f1782eec743ab1
AugustinJose1221 <augustinjose1221@gmail.com>
2020-05-25 13:01:04 +0530

    Minor Updates

--- src/resume.js
+++ src/resume.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react'
-//import './css/style.css';
+import './css/style.css';
 import { Container, Row, Col } from 'react-grid-system';
 const Resume = () => {
   return (
-    <Container>
-    <Row>
-      <Col sm={4}>
-        One of three columns
-      </Col>
-      <Col sm={4}>
-        One of three columns
-      </Col>
-      <Col sm={4}>
-        One of three columns
-      </Col>
-    </Row>
-</Container>
+    <div>
+      <section className="Header">
+        <Container>
+          <Row>
+            <Col sm={4}>
+            </Col>
+            <Col sm={4}>
+            </Col>
+            <Col sm={4}>
+            <p align="right">Home</p>
+            </Col>
+          </Row>
+        </Container>
+    </section>
+    <section className="Profile">
+      <Container>
+        <Row>
+          <Col sm={4}>
+          </Col>
+          <Col sm={4}>
+          <img src={ require('./img/Augustin.JPG') } />
+          <h1>Augustin Jose</h1>
+          </Col>
+          <Col sm={4}>
+          </Col>
+        </Row>
+      </Container>
+      </section>
+  </div>
   );
 }
 
