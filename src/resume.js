dccdc82db89fd6eaafc526e4be22ded163ac95dc
AugustinJose1221 <augustinjose1221@gmail.com>
2020-05-25 11:03:29 +0530

    Minor Updates

---
+++ src/resume.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class Resume extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm">
+            One of three columns
+          </div>
+          <div className="col-sm">
+            One of three columns
+          </div>
+          <div className="col-sm">
+            One of three columns
+        </div>
+      </div>
+    </div>
+    )
+}
+}
