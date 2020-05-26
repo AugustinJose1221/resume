335b690887518efb9cd1e6b0013836cce92be63e
AugustinJose1221 <augustinjose1221@gmail.com>
2020-05-26 20:36:14 +0530

    Updates

---
+++ precache-manifest.d40080645e5ab9ad11309ce898c3c75d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cec78a90647df096fe3e567217152c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c46699ec5b84877e1493",
+    "url": "/static/css/main.ca958f20.chunk.css"
+  },
+  {
+    "revision": "4b45f2000182db77676a",
+    "url": "/static/js/2.56e26d95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56e26d95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c46699ec5b84877e1493",
+    "url": "/static/js/main.07df3870.chunk.js"
+  },
+  {
+    "revision": "80e274ec0ec388e5e202",
+    "url": "/static/js/runtime-main.c8b434d9.js"
+  },
+  {
+    "revision": "b0eb8c23f35d5a0c53113cc1337eac06",
+    "url": "/static/media/Augustin.b0eb8c23.JPG"
+  },
+  {
+    "revision": "225bdec41acd2bb480fcaaf228ea8812",
+    "url": "/static/media/github.225bdec4.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5f706c91efb74c66a9dd6bcea280ab73",
+    "url": "/static/media/website.5f706c91.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.6b9f7d1f8a0b033258c61bfee5315bc9.js"
+  "/precache-manifest.d40080645e5ab9ad11309ce898c3c75d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
